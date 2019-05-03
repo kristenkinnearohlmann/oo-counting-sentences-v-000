a2232a4b987ffbdba3865464eef52d53674b3886
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-05-03 03:55:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -21,6 +21,9 @@ class String
 
     #Flatiron solution
     #self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    #regex, enclosed by fwd slashes, checks for (escaped) period or question mark or exclamation point
+    #delete_if method takes a block where w holds the split values, iterating through to check that there is 2 or more chars
+    #size counts the sentences
     self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
   end
 end

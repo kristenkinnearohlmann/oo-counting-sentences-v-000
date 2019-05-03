553f504f510b6f320c5f01d47ef2dee92c63746b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2019-05-03 03:52:08 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,12 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     delimiters = [". ","? ","! "]
+=======
+    delimiters = [".","?","!"]
+    binding.pry
+>>>>>>> 4a7d811301293dfb25e171ca9ef05e828c5e4fa8
     self.split(Regexp.union(delimiters)).count
   end
 end
